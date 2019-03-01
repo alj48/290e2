cc9104f18f9d6fbcbe2710b59246aa062c056524
Alison Jacobs <alison.jacobs@duke.edu>
2019-02-28 19:58:49 -0500

    worked with alina

---
+++ main.js
@@ -0,0 +1,23 @@
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    icecreamflavors: [
+      {
+        name: "cookie dough",
+        image: "images/cookiedough.jpg",
+        isshow: false,
+        descript: "full of cookie dough, vanilla based"
+      },
+      {
+        name: "coffee",
+        image: "images/coffee.jpg",
+        isshow: false,
+        descript: "xxxxx"
+      }
+
+
+    ]
+
+  }
+})
