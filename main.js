c0d406d7a9f5e127186932a880a059311cf862cd
Matthew Sclar <sclarmatthew@gmail.com>
2019-03-01 21:22:53 -0500

    Website Edit
    Fixed Website. Photos now good size. Added flavors and styles for background and colors.

--- main.js
+++ main.js
@@ -4,18 +4,60 @@ var app = new Vue({
   data: {
     icecreamflavors: [
       {
-        name: "cookie dough",
+        name: "Cookie Dough",
         image: "images/cookiedough.jpg",
         isshow: false,
-        descript: "full of cookie dough, vanilla based"
+        descript: "Vanilla ice cream, with gobs of cookiedough"
       },
       {
-        name: "coffee",
+        name: "Coffee",
         image: "images/coffee.jpg",
         isshow: false,
-        descript: "xxxxx"
-      }
+        descript: "Coffee ice cream, with dark chocolate chips"
+      },
+      {
+      name: "Mint Chocolate Chip",
+      image: "images/mintchip.jpg",
+      isshow: false,
+      descript: "Mint ice cream, with cold chocolate chips"
+    },
 
+    {
+    name: "Strawberry",
+    image: "images/strawberry.jpg",
+    isshow: false,
+    descript: "Strawberry ice cream, with bits of strawberry"
+  },
+  {
+    name: "Cookies and Cream",
+  image: "images/cookie.jpg",
+  isshow: false,
+  descript: "Rich Vanilla ice cream, with bits of oreo cookie"
+},
+{
+name: "Neopolitan",
+image: "images/neopolitan.jpg",
+isshow: false,
+descript: "Vanilla, Strawberry, and Chocolate ice cream, all in one delicious treat"
+},
+{
+  name: "Coffee Cookie Crumble",
+image: "images/coffeecookie.jpg",
+isshow: false,
+descript: "Rich Coffee ice cream, with bits of oreo cookie"
+},
+{
+  name: "Chocolate Almond",
+image: "images/chocolate-almond.jpg",
+isshow: false,
+descript: "Rich Chocolate ice cream, with bits of almonds"
+},
+{
+name: "Vanilla",
+image: "images/vanilla.jpg",
+isshow: false,
+descript: "Rich Creamy Vanilla Ice Cream"
+}
 
     ]
 
